03638155ebc5ebbb1a94d19763d2dd21e403da06
HORIGUCHI HIROKI <hiroki8.lovefootball@gmail.com>
2020-04-08 19:20:14 +0900

    make comments per a source code.  Source code is in MainActivity and EdetActivity which are in Java directory.

--- app/src/main/java/com/example/to_do_app/EditActivity.java
+++ app/src/main/java/com/example/to_do_app/EditActivity.java
@@ -41,53 +41,69 @@ public class EditActivity extends AppCompatActivity {
         }
     }
 
+//  戻るボタンが押されて、EditActivityはライフサイクルの中でonPauseに該当する。従って、onPauseをオーバーライドしてファイル保存処理と正常に保存できたときのToast処理を記述する。
     @Override
     protected void onPause() {
         super.onPause();
 
+//        ライフサイクルの順序通り、(create,start,Resume,アクティビティ実行中,onPauseの順で流れてきて、それぞれの諸条件によって処理を分けている)
+//        この処理は、オーバライドしているそれぞれのクラス階層の一個上で定義している、mNotSave変数にTrueが入っているのかFalseが入っているのかで場合わけしている。
+//        そんな中で、以下はmNotSaxe=trueのとき、すなわち、onOptionItemSelectedでボタンが押され、mNotsaveの値が書き換わったとき、returnでonPauseを終了する。
         if (mNotSave) {
             return;
         }
+//
 
-//
-//
+//      この処理は、ToDo_Titleという名前がついたEditTextを持つViewを見つけ、そこにあるテキストを変数eTxtTitleに代入している。
+//      上の処理と同様。こちらは、ToDo_detailをeTxtDetailに代入している。
+//      それぞれの変数からテキストのみを取得し、それを文字列に変換している。ちなみに、この変数の中に何が入っているのかはまだ覗いてない。。。
         EditText eTxtTitle = (EditText) findViewById(R.id.ToDo_Title);
         EditText eTxtDetail = (EditText) findViewById(R.id.ToDo_detail);
         String title = eTxtTitle.getText().toString();
         String detail = eTxtDetail.getText().toString();
-//
+//      上記２つの変数のうち、textとdetailが空の場合の処理。Toastでショートメッセージを飛ばしながら、returnでonPauseを終了する。
         if (title.isEmpty() && detail.isEmpty()) {
             Toast.makeText(this, R.string.msg_destruction, Toast.LENGTH_SHORT).show();
             return;
         }
 
-//
+//      これはmFilenameがからの場合の処理。つまり、該当ファイルが存在せず新規作成をする場合の処理。
+//      Oncreateで取得したmFilename変数がからの場合は、新しく作成時の日付からミリ秒まで取得してファイルを生成する。
         if (mFilename.isEmpty()) {
+//            ファイルを生成するために、DateクラスのSystem.currentTimeMillis()メソッドを用いて、欲しい日時を代入する。
             Date date = new Date(System.currentTimeMillis());
+//            SimpleDateFormatクラスにnewメソッドを用いてsdfオブジェクトを生成する。ここで、SimpleDateFormatの引数に日時のデータ型と日時を取得する場所を与えている。
             SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmssSSS", Locale.JAPAN);
+//            ここで変数mfilenameがsdf.txt(日付+.txt)となるように代入する。もちろん、保存を行わないといけないため、ここで処理を終わられないので、returnは記述しない。
             mFilename = sdf.format(date) + ".txt";
         }
 
-//
-//
+//      OutPutStreamとPrintWriterクラスのオブジェクトを生成し、nullを代入する。
         OutputStream out = null;
         PrintWriter writer = null;
 
-//
+//      保存処理を行う。
         try {
+//            ここでOutPutStreamクラスのオブジェクトであるout変数に、、、、this.の意味がわからん。これなんだ？？？=>「outのことを示しているのかな？？」
             out = this.openFileOutput(mFilename, Context.MODE_PRIVATE);
+//            ここではPrintWriterクラスのオブジェクトであるwriterに、PrintWriterのインスタンスを代入している。
+//            つまり、PrintWriterのインスタンスを作成するために、引数にOutputStreamWriter(引数)を与えた。
             writer = new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
-
+//          writerに、titleとdetailを書き込む。
             writer.println(title);
             writer.print(detail);
 
+//            writerとoutを閉じる。
             writer.close();
             out.close();
+
+//            例外処理。
         }catch (Exception e){
             Toast.makeText(this, "File save error!", Toast.LENGTH_LONG).show();
         }
     }
 
+//    ゴミ箱ボタンを押すためにmenuディレクトリのedit.xmlをここでセットしている。
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -95,23 +111,31 @@ public class EditActivity extends AppCompatActivity {
         return true;
     }
 
-
+//  ゴミ箱ボタンを押したときの処理。
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+//        switchで、item.getItemId()でItemIDを取得し、caseで処理を分けていく。
         switch (item.getItemId()) {
+//            このケースで、action_delというandroid:idを命名したゴミ箱ボタンが押された場合の処理を行う。
             case R.id.action_del:
+//                ここで、mFilenameが空でない、つまり、きちんとファイル名が生成されているものは削除する。
                if (!mFilename.isEmpty()){
+//                   やはり、thisの使い方がこのサンプルではよくわからない。これはこのクラスのという意味のthisなのか？
                    if(this.deleteFile(mFilename)){
+//                       削除できたらToastを飛ばしてショートメッセージを表示する。
                        Toast.makeText(this,R.string.msg_del, Toast.LENGTH_SHORT).show();
                    }
                }
+//               この処理は、deleteが成功しても失敗してもmNotSave変数をtrueに書き換えて、onPauseにて記載されている保存処理を飛ばしてアクティビティを終了する。
                mNotSave = true;
                this.finish();
                break;
+//            上記ケース以外。処理をbreakしてEditActivityのライフサイクルを終了させ、MainActivityのonResumeに遷移する。
             default:
                 break;
         }
-
+//      　おまじない？？？これはなんだ。。
+//        Fragmentを使うときにこの戻り値が何になっているかが重要らしい。これ自体は、メモリを使うのであればtrueを、使わないのであればfalseを返すためのものみたい。
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/to_do_app/MainActivity.java
+++ app/src/main/java/com/example/to_do_app/MainActivity.java
@@ -26,53 +26,78 @@ import java.util.Map;
 import java.util.List;
 
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+//    最終的にListViewに全てのファイル名を参照させ、その内容を表示するために、SimpleAdapterを使用した。
     SimpleAdapter mAdapter = null;
     List<Map<String, String>> mList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        activity_mainの画面を用意
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//       なぜかはわからないけど、ListViewに対して投稿を紐づけるためにはどうやらAdapterなるものを生成しないといけないらしい。
+//       従って、Adapterの引数にするために、ここでListクラスのオブジェクトとしてmListを定義。
         mList = new ArrayList<Map<String,String>>();
-
+//       ここでは、SimpleAdapterクラスをインスタンス 化してmAdapterに代入している。
+//       その際に、引数として、mListが使用されている。リストとしての役割を持っているmListを第二引数として入れている。
         mAdapter = new SimpleAdapter(
                 MainActivity.this,
                 mList,
+//                ここのsimple_list_itemについては他にもいくつかの種類があることがわかっている。
                 android.R.layout.simple_list_item_2,
+//                ここでは新しくstringのリストを作っている。これは、第二引数のMapのキーの配列。
                 new String[]{"title", "detail"},
+//                ここでは、新しくintのリストを作っている。引数はtext1とtext2。。。。この引数ってなんのために入れるんだっけ。。
+//                =>第三引数のレイアウトXMLファイル内のViewのid番号の配列。これについては、第三引数がどうなっているかをその都度確認する必要がありそう！
                 new int[]{android.R.id.text1, android.R.id.text2}
         );
 
+//        ここでListViewをセット。その下でmAdapterをlistにセットした。
         ListView list = findViewById(R.id.listView);
         list.setAdapter(mAdapter);
-
+//      ここでは、listのそれぞれの要素がタップされたときの処置を記述している。
+//      従って、タップが起こったときの処理をインデントを下げて記述する必要がある。
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int posision, long id) {
+//                Intentを用いて、タップされた投稿の編集画面へと遷移させる。IntentはいつものIntent.
                 Intent intent = new Intent(MainActivity.this, EditActivity.class);
+//                既に作成されている投稿の編集なので、intentにputExtraメソッドを用いてファイル名、タイトル、詳細の文字列を渡してあげる。
+//                その時に、第一引数がキーで第二引数がmListの中に入っているfilename,title,detailがキーとなっている文字列を渡す。
                 intent.putExtra("NAME", mList.get(posision).get("filename"));
                 intent.putExtra("TITLE", mList.get(posision).get("title"));
                 intent.putExtra("DETAIL", mList.get(posision).get("detail"));
+//                いつものstartActivity!!
                 startActivity(intent);
             }
         });
 
-        registerForContextMenu(list);
+//          この処理は長押し用の処理です↓
+//        registerForContextMenu(list);
     }
 
+//    ライフサイクル的に、EditActivityから戻ってきた時も、登録されたtodoを表示しないといけないので、その処理を書く。ライフサイクル的に、Resumeの位置なのでこれをオーバーライド。
     @Override
     protected void onResume() {
         super.onResume();
-
+//      mListにクリアメソッドをかけた。oncreateから更新されていないので、EditActivityで行った変更を反映するため。
         mList.clear();
 
-        String savePath = this.getFilesDir().getPath().toString();
+//        savePathに、アプリのファイル保存先ディレクトリから取得できたパスを代入する。
+//        ちなみに、thisはこの場合、Contextを表しており、MainActivityのこと。MainActivityがある階層から保存先ディレクトリまでの絶対パスを取得。
+        String savePath = this.getFilesDir().getPath();
+//        その後に、そのディレクトリの中にあるファイル名一覧を全て取得してfilesに代入する。
         File[] files = new File(savePath).listFiles();
-
+//        filesに入っているデータを全て新しい順にソート。
         Arrays.sort(files, Collections.reverseOrder());
-
+//        これらfilesの中から、保存されているfilenameとtitleとdetailを全て取り出して、mlistの中に主キーと共にmapメソッドを用いてぶち込んでいく。
+//        そのとき、以下のif文を用いて場合わけし、try,catchの例外構文を用いてそれぞれのファイルからtitleとdetailを取得する。
+//        これらをmapクラスをインスタンス化したmap変数に代入し、mListにぶち込んでいく。
         for (int i=0;i<files.length;i++) {
             String fileName = files[i].getName();
             if (files[i].isFile() && fileName.endsWith(".txt")) {
@@ -100,12 +125,15 @@ public class MainActivity extends AppCompatActivity {
                 mList.add(map);
             }
         }
-
+//        ここで、mAdapterに変更を加えたことをお知らせすると、新しいAdapterがListViewに渡されることになる。
         mAdapter.notifyDataSetChanged();
     }
 
+
+//  menuフォルダで記述したアイコンをタップしたときの処理を記述する。この時に、intentを利用して画面遷移をする。
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+//      switchでitemに対してgetItemIdメソッドを適用し、IDが取得できた場合のみ、caseでif文を指定して処理を分ける。IDがaction_addのIDの場合、画面遷移をするように処理する。
         switch (item.getItemId()) {
             case R.id.action_add:
                 Intent intent = new Intent(this, EditActivity.class);
@@ -118,6 +146,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+//    menuフォルダでmainのなかにアイコンをitemタグで作成したので、そのメニューがタップされたときの処理を記述する。戻り値にreturnが戻る。
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
